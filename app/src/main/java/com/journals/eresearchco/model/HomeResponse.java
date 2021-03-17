285954bd786df04de0c88720106878324c244d7d
anil-k <anilwesley94@gmail.com>
2021-03-17 09:52:57 +0530

    Initial commit

---
+++ app/src/main/java/com/journals/eresearchco/model/HomeResponse.java
@@ -0,0 +1,260 @@
+package com.journals.eresearchco.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class HomeResponse {
+
+
+    /**
+     * status : true
+     * cat_cnt : 9
+     * cat_details : [{"cat_id":"2","category_name":"Chemistry","page_url":"https://www.alliedacademies.org/chemistry-journals.php","cat_img":"chemistry.jpg"},{"cat_id":"3","category_name":"Clinical Sciences","page_url":"https://www.alliedacademies.org/clinical-journals.php","cat_img":"clinical.jpg"},{"cat_id":"4","category_name":"Food & Nutrition","page_url":"https://www.alliedacademies.org/food-nutrition-journals.php","cat_img":"food-and-nutrition.jpg"},{"cat_id":"5","category_name":"General Science","page_url":"https://www.alliedacademies.org/general-science-journals.php","cat_img":"general-science.jpg"},{"cat_id":"6","category_name":"Genetics & Molecular Biology","page_url":"https://www.alliedacademies.org/genetics-molecular-biology-journals.php","cat_img":"genetics-molecular-biology.jpg"},{"cat_id":"11","category_name":"Immunology & Microbiology","page_url":"https://www.alliedacademies.org/immunology-microbiology-journals.php","cat_img":"immunology-microbiology.jpg"},{"cat_id":"12","category_name":"Medical Sciences","page_url":"https://www.alliedacademies.org/medical-journals.php","cat_img":"medical.jpg"},{"cat_id":"9","category_name":"Neurology & Psychology","page_url":"https://www.alliedacademies.org/neurology-psychology-journals.php","cat_img":"neurology-psychology.png"},{"cat_id":"10","category_name":"Nursing & Health Care","page_url":"https://www.alliedacademies.org/nursing-health-care-journals.php","cat_img":"nursing-and-health-care.jpg"}]
+     * currissue_highlights : [{"sno":"15819","title":"Molecular characterization of Hymenolepsis nana infecting human in Sohag, Egypt.","author_names":"Amal Mostafa Ahmed*, Nada Abd El Fattah El Nadi, Asmaa Kamal Abd Ellah, Noha Sammer Ahmed","abstractlink":"molecular-characterization-of-hymenolepsis-nana-infecting-human-in-sohag-egypt-15819.html","pdflink":"https://www.alliedacademies.org/articles/molecular-characterization-of-hymenolepsis-nana-infecting-human-in-sohag-egypt.pdf","fulltextlink":null,"art_type":"Research Article","abstract":"Background: Hymenolepis nana is the most common cestode in humans, rats and mice. It is not clear whether H. nana found in humans is the same species found in rodents or whether they are two distinct species morphologically identical yet genetically distinct.\r\nStringive: To identify the genomic diversity of H. nana human isolates in Sohag, to determine their transmission patterns and to construct the phylogenetic tree to assess the evolutionary relationship of H. nana human and rodent isolates.\r\nMethods: 20 H. nana positive stool samples were collected from outpatients? children referred to Sohag hospitals. Genomic DNA was extracted from stool samples. Mitochondrial cytochrome C Oxidase subunits 1 (COX1) gene of H. nana human isolates were amplified, sequenced and analyzed phylogenetically. The phylogenetic trees were reconstructed by neighbor joining and maximum likelihood methods based on COX1 gene of H. nana human isolates obtained in this study and the known H. nana human and rodent isolates published in GenBank.\r\nResults: All 20 H. nana isolates were successfully amplified and sequenced at the COX1 and produced 5 different COX1 gene sequences. The NJ and ML phylogenetic tree showed almost similar topology which suggested that H. nana human isolates of present study were closely related which means they came from the same origin, were more related to H. nana infecting human especially Latin America due to migration, differed notably from rodent isolates of previous studies which support that rodent to human is less likely to happen and were different from human isolate of Australia. This is explained by evolution in Australian isolates due early separated from rest of the world. Genetic differences were found 0.6% to 4% between human isolates of present study and rodent isolates of previous studies. The annotated sequences of our Egyptian isolates were deposited in GenBank of NCBI under the following accession number; MT093851, MT093852, MT093853, MT093854 and MT093855.\r\nConclusions: The Phylogeny of the COX1 suggested that H. nana is a species complex or cryptic species (morphologically identical yet genetically distinct) and the life-cycle of H. nana that exists in Sohag is likely to Involve mainly human to human transmission.","page_num":"1","lpage_num":"7","date":"2021-03-15","vol":"6","issue":"1","rel_keyword":"articles","jshort_name":"J Parasit Dis Diagn Ther"},{"sno":"15817","title":"Ascaris lumbricoides causing parasitic infection","author_names":"Amelia Smith","abstractlink":"ascaris-lumbricoides-causing-parasitic-infection-15817.html","pdflink":"https://www.alliedacademies.org/articles/ascaris-lumbricoides-causing-parasitic-infection.pdf","fulltextlink":null,"art_type":"Editorial","abstract":"Ascaris lumbricoides is one of the most common parasites, and although infections are generally moderate, serious complications can occur. Parasitic infections impact people of all ages, and they've been linked to demographic, cultural, and sanitary-hygienic factors. The parasitic nematode Ascaris lumbricoides is one of the most widespread parasites on the planet. Vomiting, malnutrition, pneumonitis, growth retardation, and stomach pain are some of the symptoms of infection. Cholangitis, pancreatitis, appendicitis, and even masses of intestinal obstruction and, in extreme cases, perforation may all result from a serious infestation.","page_num":"1","lpage_num":"1","date":"2021-03-15","vol":"6","issue":"1","rel_keyword":"articles","jshort_name":"J Parasit Dis Diagn Ther"},{"sno":"15816","title":"Foodborne illness caused by Parasites","author_names":"Amelia Smith","abstractlink":"foodborne-illness-caused-by-parasites-15816.html","pdflink":"https://www.alliedacademies.org/articles/foodborne-illness-caused-by-parasites.pdf","fulltextlink":null,"art_type":"Editorial","abstract":"Foodborne parasites have long been a forgotten group of pathogens, since they are mostly associated with vulnerable or disadvantaged groups and have insidious, persistent symptoms rather than acute diseases. Since they are mainly associated with poor or marginalized populations and have insidious, recurring effects rather than severe diseases, foodborne parasites have long become a forgotten category of pathogens. Foodborne parasites, on the other hand, have been able to appear in diverse populations in new environments, spread by various food types, and often with unexplained symptoms as a result of global patterns.","page_num":"1","lpage_num":"1","date":"2021-03-15","vol":"6","issue":"1","rel_keyword":"articles","jshort_name":"J Parasit Dis Diagn Ther"},{"sno":"15797","title":"BIOLOGICAL FUNCTIONS OF RIBOZYMES","author_names":"Sailaja R","abstractlink":"biological-functions-of-ribozymes-15797.html","pdflink":"https://www.alliedacademies.org/articles/biological-functions-of-ribozymes.pdf","fulltextlink":null,"art_type":"Review Article","abstract":"The previous few years have seen energizing advances in understanding the design and capacity of reactant RNA. Precious stone designs of a few ribozymes have given itemized understanding into the folds of RNA particles. Models of other naturally significant RNAs have been developed dependent on primary, phylogenetic, and biochemical information. Nonetheless, numerous inquiries with respect to the synergist systems of ribozymes remain. This survey looks at the designs and conceivable reactant components of four little self-cutting RNAs: the hammerhead, fastener, hepatitis delta infection, and in vitro?chose lead-subordinate ribozymes. The association of these little impetuses is differentiated to that of bigger ribozymes, for example, the gathering I intron.","page_num":"653","lpage_num":"654","date":"2021-03-06","vol":"17","issue":"1","rel_keyword":"articles","jshort_name":"J RNAi Gene Silencing"},{"sno":"15796","title":"A systematic review of Genomic and genetic studies","author_names":"Kamran K","abstractlink":"a-systematic-review-of-genomic-and-genetic-studies-15796.html","pdflink":"https://www.alliedacademies.org/articles/a-systematic-review-of-genomic-and-genetic-studies.pdf","fulltextlink":null,"art_type":"Mini Review","abstract":"<p><span style=\"font-size: 10pt; text-align: justify;\">Data from the human genome arrangement will at last adjust numerous parts of clinical practice. It will increment through our comprehension of sickness components, and guide the advancement of new medications and remedial techniques. For the time being, notwithstanding, information on the genome will clinically affect the symptomatic capacity of clinical hereditary qualities research facilities. Sub-atomic phenotyping utilizing hereditary and genomic data will permit early and more exact forecast and analysis of infection and of sickness movement. Medication will get situated towards infection avoidance as opposed to endeavors to fix individuals at late phases of ailment.<\/span><\/p>\r\n<p class=\"MsoNormal\" style=\"margin-top:0cm;margin-right:1.9pt;margin-bottom:0cm;&#10;margin-left:5.5pt;margin-bottom:.0001pt;text-align:justify;line-height:103%\"><span lang=\"EN-US\" style=\"font-size:10.0pt;mso-bidi-font-size:11.0pt;line-height:103%\"><o:p><\/o:p><\/span><\/p>\r\n<p>&nbsp;<\/p>","page_num":"651","lpage_num":"652","date":"2021-03-06","vol":"17","issue":"1","rel_keyword":"articles","jshort_name":"J RNAi Gene Silencing"},{"sno":"15795","title":"An Overview on Single-Cell Epigenomics","author_names":"Sharukh S","abstractlink":"an-overview-on-singlecell-epigenomics-15795.html","pdflink":"https://www.alliedacademies.org/articles/an-overview-on-singlecell-epigenomics.pdf","fulltextlink":null,"art_type":"Opinion Article","abstract":"Single-cell epigenomics joins touchy epigenetic profiling, single-cell seclusion and barcoding and high-throughput sequencing to characterize epigenetic scenes across companions of single cells. Single-cell bisulfite sequencing describes the methylation scenes of uncommon or potentially heterogeneous cell populaces. Single-cell ATAC-seq (test for transposase-available chromatin with high-throughput sequencing) and single-cell Hi-C permit portrayal of the changeability in nearby and worldwide actual properties of single chromosomes. Incomplete epigenomic inclusion per single cell can be repaid by expanding test size, by computational ascription of missing qualities or by utilizing reference-populace epigenomics to evaluate epigenetic dispersions in predefined gatherings of loci","page_num":"649","lpage_num":"650","date":"2021-03-06","vol":"17","issue":"1","rel_keyword":"articles","jshort_name":"J RNAi Gene Silencing"}]
+     */
+
+    private boolean status;
+    private int cat_cnt;
+    /**
+     * cat_id : 2
+     * category_name : Chemistry
+     * page_url : https://www.alliedacademies.org/chemistry-journals.php
+     * cat_img : chemistry.jpg
+     */
+
+    private List<CatDetailsBean> cat_details;
+    /**
+     * sno : 15819
+     * title : Molecular characterization of Hymenolepsis nana infecting human in Sohag, Egypt.
+     * author_names : Amal Mostafa Ahmed*, Nada Abd El Fattah El Nadi, Asmaa Kamal Abd Ellah, Noha Sammer Ahmed
+     * abstractlink : molecular-characterization-of-hymenolepsis-nana-infecting-human-in-sohag-egypt-15819.html
+     * pdflink : https://www.alliedacademies.org/articles/molecular-characterization-of-hymenolepsis-nana-infecting-human-in-sohag-egypt.pdf
+     * fulltextlink : null
+     * art_type : Research Article
+     * abstract : Background: Hymenolepis nana is the most common cestode in humans, rats and mice. It is not clear whether H. nana found in humans is the same species found in rodents or whether they are two distinct species morphologically identical yet genetically distinct.
+     Stringive: To identify the genomic diversity of H. nana human isolates in Sohag, to determine their transmission patterns and to construct the phylogenetic tree to assess the evolutionary relationship of H. nana human and rodent isolates.
+     Methods: 20 H. nana positive stool samples were collected from outpatients? children referred to Sohag hospitals. Genomic DNA was extracted from stool samples. Mitochondrial cytochrome C Oxidase subunits 1 (COX1) gene of H. nana human isolates were amplified, sequenced and analyzed phylogenetically. The phylogenetic trees were reconstructed by neighbor joining and maximum likelihood methods based on COX1 gene of H. nana human isolates obtained in this study and the known H. nana human and rodent isolates published in GenBank.
+     Results: All 20 H. nana isolates were successfully amplified and sequenced at the COX1 and produced 5 different COX1 gene sequences. The NJ and ML phylogenetic tree showed almost similar topology which suggested that H. nana human isolates of present study were closely related which means they came from the same origin, were more related to H. nana infecting human especially Latin America due to migration, differed notably from rodent isolates of previous studies which support that rodent to human is less likely to happen and were different from human isolate of Australia. This is explained by evolution in Australian isolates due early separated from rest of the world. Genetic differences were found 0.6% to 4% between human isolates of present study and rodent isolates of previous studies. The annotated sequences of our Egyptian isolates were deposited in GenBank of NCBI under the following accession number; MT093851, MT093852, MT093853, MT093854 and MT093855.
+     Conclusions: The Phylogeny of the COX1 suggested that H. nana is a species complex or cryptic species (morphologically identical yet genetically distinct) and the life-cycle of H. nana that exists in Sohag is likely to Involve mainly human to human transmission.
+     * page_num : 1
+     * lpage_num : 7
+     * date : 2021-03-15
+     * vol : 6
+     * issue : 1
+     * rel_keyword : articles
+     * jshort_name : J Parasit Dis Diagn Ther
+     */
+
+    private List<CurrissueHighlightsBean> currissue_highlights;
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public int getCat_cnt() {
+        return cat_cnt;
+    }
+
+    public void setCat_cnt(int cat_cnt) {
+        this.cat_cnt = cat_cnt;
+    }
+
+    public List<CatDetailsBean> getCat_details() {
+        return cat_details;
+    }
+
+    public void setCat_details(List<CatDetailsBean> cat_details) {
+        this.cat_details = cat_details;
+    }
+
+    public List<CurrissueHighlightsBean> getCurrissue_highlights() {
+        return currissue_highlights;
+    }
+
+    public void setCurrissue_highlights(List<CurrissueHighlightsBean> currissue_highlights) {
+        this.currissue_highlights = currissue_highlights;
+    }
+
+    public static class CatDetailsBean {
+        private String cat_id;
+        private String category_name;
+        private String page_url;
+        private String cat_img;
+
+        public String getCat_id() {
+            return cat_id;
+        }
+
+        public void setCat_id(String cat_id) {
+            this.cat_id = cat_id;
+        }
+
+        public String getCategory_name() {
+            return category_name;
+        }
+
+        public void setCategory_name(String category_name) {
+            this.category_name = category_name;
+        }
+
+        public String getPage_url() {
+            return page_url;
+        }
+
+        public void setPage_url(String page_url) {
+            this.page_url = page_url;
+        }
+
+        public String getCat_img() {
+            return cat_img;
+        }
+
+        public void setCat_img(String cat_img) {
+            this.cat_img = cat_img;
+        }
+    }
+
+    public static class CurrissueHighlightsBean {
+        private String sno;
+        private String title;
+        private String author_names;
+        private String abstractlink;
+        private String pdflink;
+        private String fulltextlink;
+        private String art_type;
+        @SerializedName("abstract")
+        private String abstractX;
+        private String page_num;
+        private String lpage_num;
+        private String date;
+        private String vol;
+        private String issue;
+        private String rel_keyword;
+        private String jshort_name;
+
+        public String getSno() {
+            return sno;
+        }
+
+        public void setSno(String sno) {
+            this.sno = sno;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getAuthor_names() {
+            return author_names;
+        }
+
+        public void setAuthor_names(String author_names) {
+            this.author_names = author_names;
+        }
+
+        public String getAbstractlink() {
+            return abstractlink;
+        }
+
+        public void setAbstractlink(String abstractlink) {
+            this.abstractlink = abstractlink;
+        }
+
+        public String getPdflink() {
+            return pdflink;
+        }
+
+        public void setPdflink(String pdflink) {
+            this.pdflink = pdflink;
+        }
+
+        public String getFulltextlink() {
+            return fulltextlink;
+        }
+
+        public void setFulltextlink(String fulltextlink) {
+            this.fulltextlink = fulltextlink;
+        }
+
+        public String getArt_type() {
+            return art_type;
+        }
+
+        public void setArt_type(String art_type) {
+            this.art_type = art_type;
+        }
+
+        public String getAbstractX() {
+            return abstractX;
+        }
+
+        public void setAbstractX(String abstractX) {
+            this.abstractX = abstractX;
+        }
+
+        public String getPage_num() {
+            return page_num;
+        }
+
+        public void setPage_num(String page_num) {
+            this.page_num = page_num;
+        }
+
+        public String getLpage_num() {
+            return lpage_num;
+        }
+
+        public void setLpage_num(String lpage_num) {
+            this.lpage_num = lpage_num;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        public String getVol() {
+            return vol;
+        }
+
+        public void setVol(String vol) {
+            this.vol = vol;
+        }
+
+        public String getIssue() {
+            return issue;
+        }
+
+        public void setIssue(String issue) {
+            this.issue = issue;
+        }
+
+        public String getRel_keyword() {
+            return rel_keyword;
+        }
+
+        public void setRel_keyword(String rel_keyword) {
+            this.rel_keyword = rel_keyword;
+        }
+
+        public String getJshort_name() {
+            return jshort_name;
+        }
+
+        public void setJshort_name(String jshort_name) {
+            this.jshort_name = jshort_name;
+        }
+    }
+}
